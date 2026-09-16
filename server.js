ebba073a81ea7426e6d0c01bdb2af981a77673e0
twidyshop <3twidy@gmail.com>
2026-09-16 11:42:43 +0700

    Add robots.txt and sitemap.xml endpoints

--- server.js
+++ server.js
@@ -772,6 +772,67 @@ app.post('/api/digiflazz-webhook', (req, res) => {
     }
 });
 
+// --- FITUR SEO: ROBOTS.TXT & SITEMAP.XML ---
+app.get('/robots.txt', (req, res) => {
+    res.type('text/plain');
+    res.send(`User-agent: *
+Allow: /
+Sitemap: https://tlibrary.my.id/sitemap.xml`);
+});
+
+app.get('/sitemap.xml', (req, res) => {
+    const digitalProducts = readDigitalDB();
+    const baseUrl = 'https://tlibrary.my.id';
+    const generateSlug = (text) => text.toLowerCase().replace(/[^\w ]+/g, '').replace(/ +/g, '-');
+
+    let xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>${baseUrl}/</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>${baseUrl}/topup</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.9</priority>
+    </url>
+    <url>
+        <loc>${baseUrl}/riwayat</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>${baseUrl}/promo</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>${baseUrl}/panduan</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.7</priority>
+    </url>`;
+
+    if (digitalProducts && digitalProducts.length > 0) {
+        digitalProducts.forEach(p => {
+            if (p.name) {
+                const slug = generateSlug(p.name);
+                xml += `
+    <url>
+        <loc>${baseUrl}/${slug}</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+    </url>`;
+            }
+        });
+    }
+
+    xml += `\n</urlset>`;
+    res.header('Content-Type', 'application/xml');
+    res.send(xml);
+});
+// --- END FITUR SEO ---
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
